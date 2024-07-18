18ccf6a25a319517b7d7b99a84ffa970dc654273
Bodunrin <yekinibodunrin@gmail.com>
2024-07-18 11:09:45 +0200

    OG prop vendor

--- proprietary/vendor/bin/idlefast.sh
+++
@@ -1,5 +0,0 @@
-#!/vendor/bin/sh
-# SPDX-FileCopyrightText: 2016-2023 Unisoc (Shanghai) Technologies Co., Ltd
-# SPDX-License-Identifier: LicenseRef-Unisoc-General-1.0
-
-echo idle_fast > /sys/block/zram0/writeback
